98efb78000f8d0ec12cad8cd00b6577420f48db1
flinders-cims <flinderscims@outlook.com>
2024-09-05 11:02:42 +0930

    added research changes  -  CRUD research

---
+++ src/main/java/com/flinders/cims/model/Research.java
@@ -0,0 +1,103 @@
+package com.flinders.cims.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "researches")
+public class Research {
+
+    @Id
+    @Column(name = "research_id", nullable = false, unique = true)
+    private int researchId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "expected_end_date")
+    private LocalDate expectedEndDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ResearchStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)  // Reference to the User entity
+    private User user;
+
+    // Getters and Setters
+
+    public int getResearchId() {
+        return researchId;
+    }
+
+    public void setResearchId(int researchId) {
+        this.researchId = researchId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpectedEndDate() {
+        return expectedEndDate;
+    }
+
+    public void setExpectedEndDate(LocalDate expectedEndDate) {
+        this.expectedEndDate = expectedEndDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public ResearchStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ResearchStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
