e7c10f758e04d48b8bb0153fded890ecb4e16073
geor0380 <flinderscims@outlook.com>
2024-09-25 13:09:39 +0930

    CRUD chemical changes

---
+++ src/main/java/com/flinders/cims/repository/ChemicalRepository.java
@@ -0,0 +1,12 @@
+package com.flinders.cims.repository;
+
+import com.flinders.cims.model.Chemical;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChemicalRepository extends JpaRepository<Chemical, Integer> {
+    Optional<Chemical> findBySystematicName(String systematicName);
+}
